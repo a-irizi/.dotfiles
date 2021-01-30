6c8560c5db7a741db1b56b5642a9c36c54141958
Anass IRIZI <irizi.anass.00@gmail.com>
2021-01-30 03:23:39 +0100

    Added the lsp-mode package
        * Used C-c l as the lsp-keymap-mode keybinding
        * enabled which-key integration with lsp-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -209,13 +209,20 @@
   :config
   (global-evil-surround-mode 1))
 
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :config
+  (lsp-enable-which-key-integration t))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(evil-surround autopair undo-fu-session undo-fu general hydra selectrum-prescient company-prescient ivy-prescient prescient flycheck evil-collection magit doom-themes helpful all-the-icons-ivy all-the-icons-ivy-rich counsel ivy-rich which-key use-package rainbow-delimiters ivy evil doom-modeline)))
+   '(lsp-mode evil-surround autopair undo-fu-session undo-fu general hydra selectrum-prescient company-prescient ivy-prescient prescient flycheck evil-collection magit doom-themes helpful all-the-icons-ivy all-the-icons-ivy-rich counsel ivy-rich which-key use-package rainbow-delimiters ivy evil doom-modeline)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
