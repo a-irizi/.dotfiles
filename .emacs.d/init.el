92aa1ae0eca732efad10629e4a6b383e49b2d81f
Anass IRIZI <irizi.anass.00@gmail.com>
2021-02-03 00:02:37 +0100

    Text automaticaly inserted by custom

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -282,7 +282,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lsp-mode evil-surround autopair undo-fu-session undo-fu general hydra selectrum-prescient company-prescient ivy-prescient prescient flycheck evil-collection magit doom-themes helpful all-the-icons-ivy all-the-icons-ivy-rich counsel ivy-rich which-key use-package rainbow-delimiters ivy evil doom-modeline)))
+   '(company-box company-c-headers yasnippet-snippets yasnippet company-mode ccls lsp-ivy lsp-ui lsp-mode evil-surround autopair undo-fu-session undo-fu general hydra selectrum-prescient company-prescient ivy-prescient prescient flycheck evil-collection magit doom-themes helpful all-the-icons-ivy all-the-icons-ivy-rich counsel ivy-rich which-key use-package rainbow-delimiters ivy evil doom-modeline)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
