390caf6ceed45e399a625ccc95cfb7cde5c6f145
a-irizi <irizi.anass.00@gmail.com>
2022-02-27 16:18:15 +0100

    Add setup commands of mpv in setup.sh

--- setup.sh
+++ setup.sh
@@ -9,5 +9,12 @@ then
 fi
 ln -s $cwd/.config/yt-dlp/config $HOME/.config/yt-dlp/config
 
+# mpv
+if [[ ! -d "$HOME/.config/mpv" ]]
+then
+	mkdir -p "~/.config/mpv"
+fi
+ln -s $cwd/.config/mpv/mpv.conf $HOME/.config/mpv/mpv.conf
+
 # .personal_aliases
 ln -s $cwd/.personal_aliases $HOME/.personal_aliases
