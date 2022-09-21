fcac5e2519acb10b1e7d0fd54562df99b9cfec94
a-irizi <irizi.anass.00@gmail.com>
2022-09-21 23:17:45 +0100

    Add bspwm and sxhkd config

---
+++ .config/bspwm/autostart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pgrep -x sxhkd > /dev/null || sxhkd &
+picom -fb --experimental-backends --backend glx&
+numlockx &
+setxkbmap -option "ctrl:swapcaps" &
+nitrogen --restore &
+xss-lock --transfer-sleep-lock -- betterlockscreen -l &
+$HOME/.config/polybar/launch.sh &
