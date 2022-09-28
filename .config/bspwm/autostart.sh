1732325b3b73f0cc2d392560011fd3eb5a8d2038
a-irizi <irizi.anass.00@gmail.com>
2022-09-28 01:39:19 +0100

    Create function to autostart each program
    instead of directly starting each program, check if the program is not
    runnig already and the run it.

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-pgrep -x sxhkd > /dev/null || sxhkd &
-blueberry-tray &
-nm-applet &
-dunst &
-picom -fb --experimental-backends --backend glx&
+function run {
+  if ! pgrep -x $1 > /dev/null ; then
+    $@ &
+  fi
+}
+
+run sxhkd &
+run blueberry-tray &
+run nm-applet &
+run dunst &
+run picom -fb --experimental-backends --backend glx&
 numlockx &
 setxkbmap -option "ctrl:swapcaps" &
 nitrogen --restore &
