bb98a567f208f592b978252b09040fa4fd08ccbe
a-irizi <irizi.anass.00@gmail.com>
2022-10-16 23:59:38 +0100

    Changed colorscheme to dracula

--- .config/nvim/after/plugin/colorscheme.rc.lua
+++ .config/nvim/after/plugin/colorscheme.rc.lua
@@ -1,41 +1,15 @@
-local status, nightfox = pcall(require, "nightfox")
+local status, colorscheme = pcall(require, "dracula")
 if not status then
 	return
 end
-
-nightfox.setup({
-	options = {
-		-- Compiled file's destination location
-		compile_path = vim.fn.stdpath("cache") .. "/nightfox",
-		compile_file_suffix = "_compiled", -- Compiled file suffix
-		transparent = true, -- Disable setting background
-		terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-		dim_inactive = true, -- Non focused panes set to alternative background
-		styles = { -- Style to be applied to different syntax groups
-			comments = "italic", -- Value is any valid attr-list value `:help attr-list`
-			conditionals = "bold,italic",
-			constants = "bold",
-			functions = "bold,italic",
-			keywords = "bold",
-			numbers = "NONE",
-			operators = "NONE",
-			strings = "NONE",
-			types = "bold,italic",
-			variables = "italic",
-		},
-		inverse = { -- Inverse highlight for different types
-			match_paren = false,
-			visual = false,
-			search = false,
-		},
-		modules = { -- List of various plugins and additional options
-			-- ...
-		},
-	},
-	palettes = {},
-	specs = {},
-	groups = {},
+colorscheme.setup({
+	-- show the '~' characters after the end of buffers
+	show_end_of_buffer = true, -- default false
+	-- use transparent background
+	transparent_bg = true, -- default false
+	-- set italic comment
+	italic_comment = true, -- default false
 })
 
-vim.o.termguicolors = true
-vim.cmd("colorscheme nightfox")
+vim.g.termguicolors = true
+vim.cmd([[colorscheme dracula]])
