c54423b805f9322bdfb705888da237c1d5ed103a
a-irizi <irizi.anass.00@gmail.com>
2022-11-13 21:24:42 +0100

    Change lspsaga config
    * add escape to the keys that quit the popups
    * use "go" instead of <leader>o
    * use "gh" to show diagnostics

--- .config/nvim/after/plugin/lspsaga.rc.lua
+++ .config/nvim/after/plugin/lspsaga.rc.lua
@@ -12,11 +12,11 @@ saga.init_lsp_saga({
 		vsplit = "v",
 		split = "s",
 		tabe = "t",
-		quit = { "<C-g>", "q" },
+		quit = { "<esc>", "<C-g>", "q" },
 		scroll_down = "<C-f>",
 		scroll_up = "<C-b>", -- quit can be a table
 	},
-	rename_action_quit = "<C-g>",
+	rename_action_quit = "<esc>",
 })
 
 local keymap = vim.keymap.set
@@ -24,7 +24,7 @@ local opts = { silent = true }
 -- Lsp finder find the symbol definition implement reference
 -- when you use action in finder like open vsplit then you can
 -- use <C-t> to jump back
-keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", opts)
+keymap("n", "gd", "<cmd>Lspsaga lsp_finder<CR>", opts)
 
 -- Code action
 keymap("n", "ga", "<cmd>Lspsaga code_action<CR>", opts)
@@ -33,11 +33,8 @@ keymap("v", "ga", "<cmd><C-u>Lspsaga range_code_action<CR>", opts)
 -- Rename
 keymap("n", "gr", "<cmd>Lspsaga rename<CR>", opts)
 
--- Definition preview
-keymap("n", "gd", "<cmd>Lspsaga preview_definition<CR>", opts)
-
 -- Show line diagnostics
-keymap("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
+keymap("n", "gh", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
 
 -- Show cursor diagnostic
 keymap("n", "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
@@ -55,15 +52,7 @@ keymap("n", "]E", function()
 end, opts)
 
 -- Outline
-keymap("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts)
+keymap("n", "go", "<cmd>LSoutlineToggle<CR>", opts)
 
 -- Hover Doc
 keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
-
--- Float terminal
-keymap("n", "<A-d>", "<cmd>Lspsaga open_floaterm<CR>", opts)
--- if you want pass somc cli command into terminal you can do like this
--- open lazygit in lspsaga float terminal
--- keymap("n", "<A-d>", "<cmd>Lspsaga open_floaterm lazygit<CR>", opts)
--- close floaterm
-keymap("t", "<A-d>", [[<C-\><C-n><cmd>Lspsaga close_floaterm<CR>]], opts)
